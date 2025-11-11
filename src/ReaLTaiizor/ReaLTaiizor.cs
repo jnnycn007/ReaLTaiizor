134c37dbea678639d301916326b8663747d9d512
Taiizor <41683699+Taiizor@users.noreply.github.com>
2025-11-12 01:45:36 +0300

    Bump version to 3.8.1.5
    Updated version number in ReaLTaiizor.cs and ReaLTaiizor.csproj to 3.8.1.5 for the latest release.

--- src/ReaLTaiizor/ReaLTaiizor.cs
+++ src/ReaLTaiizor/ReaLTaiizor.cs
@@ -14,7 +14,7 @@ using System.Windows.Forms;
 //     Website: www.vegalya.com
 //     Created: 15.May.2019
 //     Changed: 12.Nov.2025
-//     Version: 3.8.1.4
+//     Version: 3.8.1.5
 //
 // |---------DO-NOT-REMOVE---------|
 
