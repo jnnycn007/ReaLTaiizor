26b551c2b146ea1ce41d7999e09291f5bc087b26
Taiizor <41683699+Taiizor@users.noreply.github.com>
2026-04-06 11:01:06 +0300

    Update version numbers to 3.8.1.7 / 1.5.5.9
    Bump library and CR assembly versions for a patch release. Updated ReaLTaiizor.cs comment version from 3.8.1.6 to 3.8.1.7, and ReaLTaiizor_CR/Properties/AssemblyInfo.cs description and AssemblyVersion/AssemblyFileVersion from 1.5.5.8 to 1.5.5.9. No functional changes.

--- src/ReaLTaiizor/ReaLTaiizor.cs
+++ src/ReaLTaiizor/ReaLTaiizor.cs
@@ -14,7 +14,7 @@ using System.Windows.Forms;
 //     Website: www.vegalya.com
 //     Created: 15.May.2019
 //     Changed: 06.Apr.2026
-//     Version: 3.8.1.6
+//     Version: 3.8.1.7
 //
 // |---------DO-NOT-REMOVE---------|
 
