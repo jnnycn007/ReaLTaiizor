06ec38f50f581096683c1808f10bc5b597427df7
Taiizor <41683699+Taiizor@users.noreply.github.com>
2026-04-09 19:11:46 +0300

    Add enable/disable for control box buttons
    Introduce three new properties (EnableMinimizeButton, EnableMaximizeButton, EnableCloseButton) defaulting to true and calling Invalidate() on set so the control repaints when toggled. Update OnMouseDown to skip minimize/maximize/close actions when respective flags are false. Update painting logic to render disabled state (uses a semi-transparent brush), avoid hover gradient/highlight for disabled buttons, and switch cursor to Default when a hovered button is disabled.

--- src/ReaLTaiizor/Controls/ControlBox/ThunderControlBox.cs
+++ src/ReaLTaiizor/Controls/ControlBox/ThunderControlBox.cs
@@ -26,6 +26,36 @@ namespace ReaLTaiizor.Controls
             }
         } = true;
 
+        public bool EnableMinimizeButton
+        {
+            get;
+            set
+            {
+                field = value;
+                Invalidate();
+            }
+        } = true;
+
+        public bool EnableMaximizeButton
+        {
+            get;
+            set
+            {
+                field = value;
+                Invalidate();
+            }
+        } = true;
+
+        public bool EnableCloseButton
+        {
+            get;
+            set
+            {
+                field = value;
+                Invalidate();
+            }
+        } = true;
+
         #endregion
 
         private MouseStateThunder State = MouseStateThunder.None;
@@ -60,11 +90,11 @@ namespace ReaLTaiizor.Controls
         protected override void OnMouseDown(MouseEventArgs e)
         {
             base.OnMouseDown(e);
-            if (e.Location.X is > 0 and < 20)
+            if (e.Location.X is > 0 and < 20 && EnableMinimizeButton)
             {
                 FindForm().WindowState = FormWindowState.Minimized;
             }
-            else if (e.Location.X is > 25 and < 45)
+            else if (e.Location.X is > 25 and < 45 && EnableMaximizeButton)
             {
                 if (FindForm().WindowState == FormWindowState.Normal)
                 {
@@ -75,7 +105,7 @@ namespace ReaLTaiizor.Controls
                     FindForm().WindowState = FormWindowState.Normal;
                 }
             }
-            else if (e.Location.X is > 50 and < 70)
+            else if (e.Location.X is > 50 and < 70 && EnableCloseButton)
             {
                 FindForm().Close();
             }
@@ -123,6 +153,10 @@ namespace ReaLTaiizor.Controls
             G.SmoothingMode = SmoothingMode.HighQuality;
             Font mf = new("Marlett", 9);
             SolidBrush mfb = new(ForeColor);
+            SolidBrush mfbDisabled = new(Color.FromArgb(80, ForeColor));
+            SolidBrush minBrush = EnableMinimizeButton ? mfb : mfbDisabled;
+            SolidBrush maxBrush = EnableMaximizeButton ? mfb : mfbDisabled;
+            SolidBrush clsBrush = EnableCloseButton ? mfb : mfbDisabled;
             Pen P1 = new(Color.FromArgb(21, 21, 21), 1);
             Color C1 = Color.FromArgb(66, 67, 70);
             Color C2 = Color.FromArgb(43, 44, 48);
@@ -137,139 +171,152 @@ namespace ReaLTaiizor.Controls
                     mlgb = new(MinBtn, C1, C2, 90);
                     G.FillPath(mlgb, GP1);
                     G.DrawPath(P1, GP1);
-                    G.DrawString("0", mf, mfb, 4, 4);
+                    G.DrawString("0", mf, minBrush, 4, 4);
 
                     if (FindForm().WindowState == FormWindowState.Normal)
                     {
                         G.FillPath(mlgb, GP2);
                         G.DrawPath(P1, GP2);
-                        G.DrawString("1", mf, mfb, 28, 4);
+                        G.DrawString("1", mf, maxBrush, 28, 4);
                     }
                     else
                     {
                         G.FillPath(mlgb, GP2);
                         G.DrawPath(P1, GP2);
-                        G.DrawString("2", mf, mfb, 28, 4);
+                        G.DrawString("2", mf, maxBrush, 28, 4);
                     }
 
                     G.FillPath(mlgb, GP3);
                     G.DrawPath(P1, GP3);
-                    G.DrawString("r", mf, mfb, 52, 4);
+                    G.DrawString("r", mf, clsBrush, 52, 4);
                     break;
                 case MouseStateThunder.Over:
                     if (x is > 0 and < 20)
                     {
-                        mlgb = new(MinBtn, Color.FromArgb(100, C1), Color.FromArgb(100, C2), 90);
+                        if (EnableMinimizeButton)
+                        {
+                            mlgb = new(MinBtn, Color.FromArgb(100, C1), Color.FromArgb(100, C2), 90);
+                        }
+                        else
+                        {
+                            mlgb = new(MinBtn, C1, C2, 90);
+                        }
                         G.FillPath(mlgb, GP1);
                         G.DrawPath(P1, GP1);
-                        G.DrawString("0", mf, mfb, 4, 4);
+                        G.DrawString("0", mf, minBrush, 4, 4);
 
                         if (FindForm().WindowState == FormWindowState.Normal)
                         {
                             mlgb = new(MaxBtn, C1, C2, 90);
                             G.FillPath(mlgb, DrawThunder.RoundRect(MaxBtn, 4));
                             G.DrawPath(P1, GP2);
-                            G.DrawString("1", mf, mfb, 28, 4);
+                            G.DrawString("1", mf, maxBrush, 28, 4);
                         }
                         else
                         {
                             mlgb = new(MaxBtn, C1, C2, 90);
                             G.FillPath(mlgb, DrawThunder.RoundRect(MaxBtn, 4));
                             G.DrawPath(P1, GP2);
-                            G.DrawString("2", mf, mfb, 28, 4);
+                            G.DrawString("2", mf, maxBrush, 28, 4);
                         }
 
                         mlgb = new(ClsBtn, C1, C2, 90);
                         G.FillPath(mlgb, DrawThunder.RoundRect(ClsBtn, 4));
                         G.DrawPath(P1, GP3);
-                        G.DrawString("r", mf, mfb, 52, 4);
+                        G.DrawString("r", mf, clsBrush, 52, 4);
 
-                        Cursor = Cursors.Hand;
+                        Cursor = EnableMinimizeButton ? Cursors.Hand : Cursors.Default;
                     }
                     else if (x is > 25 and < 45)
                     {
                         mlgb = new(MinBtn, C1, C2, 90);
                         G.FillPath(mlgb, GP1);
                         G.DrawPath(P1, GP1);
-                        G.DrawString("0", mf, mfb, 4, 4);
+                        G.DrawString("0", mf, minBrush, 4, 4);
 
                         if (FindForm().WindowState == FormWindowState.Normal)
                         {
-                            mlgb = new(MaxBtn, Color.FromArgb(100, C1), Color.FromArgb(100, C2), 90);
+                            mlgb = EnableMaximizeButton
+                                ? new(MaxBtn, Color.FromArgb(100, C1), Color.FromArgb(100, C2), 90)
+                                : new(MaxBtn, C1, C2, 90);
                             G.FillPath(mlgb, GP2);
                             G.DrawPath(P1, GP2);
-                            G.DrawString("1", mf, mfb, 28, 4);
+                            G.DrawString("1", mf, maxBrush, 28, 4);
                         }
                         else
                         {
-                            mlgb = new(MaxBtn, Color.FromArgb(100, C1), Color.FromArgb(100, C2), 90);
+                            mlgb = EnableMaximizeButton
+                                ? new(MaxBtn, Color.FromArgb(100, C1), Color.FromArgb(100, C2), 90)
+                                : new(MaxBtn, C1, C2, 90);
                             G.FillPath(mlgb, GP2);
                             G.DrawPath(P1, GP2);
-                            G.DrawString("2", mf, mfb, 28, 4);
+                            G.DrawString("2", mf, maxBrush, 28, 4);
                         }
 
                         mlgb = new(ClsBtn, C1, C2, 90);
                         G.FillPath(mlgb, GP3);
                         G.DrawPath(P1, GP3);
-                        G.DrawString("r", mf, mfb, 52, 4);
+                        G.DrawString("r", mf, clsBrush, 52, 4);
 
-                        Cursor = Cursors.Hand;
+                        Cursor = EnableMaximizeButton ? Cursors.Hand : Cursors.Default;
                     }
                     else if (x is > 50 and < 70)
                     {
                         mlgb = new(MinBtn, C1, C2, 90);
                         G.FillPath(mlgb, GP1);
                         G.DrawPath(P1, GP1);
-                        G.DrawString("0", mf, mfb, 4, 4);
+                        G.DrawString("0", mf, minBrush, 4, 4);
 
                         if (FindForm().WindowState == FormWindowState.Normal)
                         {
                             mlgb = new(MaxBtn, C1, C2, 90);
                             G.FillPath(mlgb, GP2);
                             G.DrawPath(P1, GP2);
-                            G.DrawString("1", mf, mfb, 28, 4);
+                            G.DrawString("1", mf, maxBrush, 28, 4);
                         }
                         else
                         {
                             mlgb = new(MaxBtn, C1, C2, 90);
                             G.FillPath(mlgb, GP2);
                             G.DrawPath(P1, GP2);
-                            G.DrawString("2", mf, mfb, 28, 4);
+                            G.DrawString("2", mf, maxBrush, 28, 4);
                         }
 
-                        mlgb = new(ClsBtn, Color.FromArgb(100, C1), Color.FromArgb(100, C2), 90);
+                        mlgb = EnableCloseButton
+                            ? new(ClsBtn, Color.FromArgb(100, C1), Color.FromArgb(100, C2), 90)
+                            : new(ClsBtn, C1, C2, 90);
                         G.FillPath(mlgb, GP3);
                         G.DrawPath(P1, GP3);
-                        G.DrawString("r", mf, mfb, 52, 4);
+                        G.DrawString("r", mf, clsBrush, 52, 4);
 
-                        Cursor = Cursors.Hand;
+                        Cursor = EnableCloseButton ? Cursors.Hand : Cursors.Default;
                     }
                     else
                     {
                         mlgb = new(MinBtn, C1, C2, 90);
                         G.FillPath(mlgb, GP1);
                         G.DrawPath(P1, GP1);
-                        G.DrawString("0", mf, mfb, 4, 4);
+                        G.DrawString("0", mf, minBrush, 4, 4);
 
                         if (FindForm().WindowState == FormWindowState.Normal)
                         {
                             LinearGradientBrush lgb1 = new(MaxBtn, C1, C2, 90);
                             G.FillPath(lgb1, GP2);
                             G.DrawPath(P1, GP2);
-                            G.DrawString("1", mf, mfb, 28, 4);
+                            G.DrawString("1", mf, maxBrush, 28, 4);
                         }
                         else
                         {
                             LinearGradientBrush lgb1 = new(MaxBtn, C1, C2, 90);
                             G.FillPath(lgb1, GP2);
                             G.DrawPath(P1, GP2);
-                            G.DrawString("2", mf, mfb, 28, 4);
+                            G.DrawString("2", mf, maxBrush, 28, 4);
                         }
 
                         LinearGradientBrush lgb2 = new(ClsBtn, C1, C2, 90);
                         G.FillPath(lgb2, GP3);
                         G.DrawPath(P1, GP3);
-                        G.DrawString("r", mf, mfb, 52, 4);
+                        G.DrawString("r", mf, clsBrush, 52, 4);
 
                         Cursor = Cursors.Default;
                     }
@@ -278,27 +325,27 @@ namespace ReaLTaiizor.Controls
                     mlgb = new(MinBtn, C1, C2, 90);
                     G.FillPath(mlgb, GP1);
                     G.DrawPath(P1, GP1);
-                    G.DrawString("0", mf, mfb, 4, 4);
+                    G.DrawString("0", mf, minBrush, 4, 4);
 
                     if (FindForm().WindowState == FormWindowState.Normal)
                     {
                         mlgb = new(MaxBtn, C1, C2, 90);
                         G.FillPath(mlgb, GP2);
                         G.DrawPath(P1, GP2);
-                        G.DrawString("1", mf, mfb, 28, 4);
+                        G.DrawString("1", mf, maxBrush, 28, 4);
                     }
                     else
                     {
                         mlgb = new(MaxBtn, C1, C2, 90);
                         G.FillPath(mlgb, GP2);
                         G.DrawPath(P1, GP2);
-                        G.DrawString("2", mf, mfb, 28, 4);
+                        G.DrawString("2", mf, maxBrush, 28, 4);
                     }
 
                     mlgb = new(ClsBtn, C1, C2, 90);
                     G.FillPath(mlgb, GP3);
                     G.DrawPath(P1, GP3);
-                    G.DrawString("r", mf, mfb, 52, 4);
+                    G.DrawString("r", mf, clsBrush, 52, 4);
 
                     Cursor = Cursors.Default;
                     break;
@@ -306,27 +353,27 @@ namespace ReaLTaiizor.Controls
                     mlgb = new(MinBtn, C1, C2, 90);
                     G.FillPath(mlgb, GP1);
                     G.DrawPath(P1, GP1);
-                    G.DrawString("0", mf, mfb, 4, 4);
+                    G.DrawString("0", mf, minBrush, 4, 4);
 
                     if (FindForm().WindowState == FormWindowState.Normal)
                     {
                         mlgb = new(MaxBtn, C1, C2, 90);
                         G.FillPath(mlgb, GP2);
                         G.DrawPath(P1, GP2);
-                        G.DrawString("1", mf, mfb, 28, 4);
+                        G.DrawString("1", mf, maxBrush, 28, 4);
                     }
                     else
                     {
                         mlgb = new(MaxBtn, C1, C2, 90);
                         G.FillPath(mlgb, GP2);
                         G.DrawPath(P1, GP2);
-                        G.DrawString("2", mf, mfb, 28, 4);
+                        G.DrawString("2", mf, maxBrush, 28, 4);
                     }
 
                     mlgb = new(ClsBtn, C1, C2, 90);
                     G.FillPath(mlgb, GP3);
                     G.DrawPath(P1, GP3);
-                    G.DrawString("r", mf, mfb, 52, 4);
+                    G.DrawString("r", mf, clsBrush, 52, 4);
 
                     Cursor = Cursors.Default;
                     break;
