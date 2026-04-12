b358b3ee29392ced34178bde2a30a4f0ffcae0a3
Taiizor <41683699+Taiizor@users.noreply.github.com>
2026-04-12 19:58:23 +0300

    Bump version to 3.8.1.9
    Update project version from 3.8.1.8 to 3.8.1.9. Adjusted the version comment in ReaLTaiizor.cs and updated the <Version> property in ReaLTaiizor.csproj (AssemblyVersion continues to use $(Version)).

--- src/ReaLTaiizor/ReaLTaiizor.cs
+++ src/ReaLTaiizor/ReaLTaiizor.cs
@@ -14,7 +14,7 @@ using System.Windows.Forms;
 //     Website: www.vegalya.com
 //     Created: 15.May.2019
 //     Changed: 12.Apr.2026
-//     Version: 3.8.1.8
+//     Version: 3.8.1.9
 //
 // |---------DO-NOT-REMOVE---------|
 
